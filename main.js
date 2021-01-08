9983dda7386daced9d63d61aa0de662043213216
Robby Polana <rpolana@squarespace.com>
2021-01-07 23:02:57 -0700

    exitCode false by default

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const defaultConfig = {
   s3EndPoint: "ceph.squarespace.net",
   s3AccessKey: process.env.S3_ACCESS_KEY || "",
   s3SecretKey: process.env.S3_SECRET_KEY || "",
-  exitCode: true,
+  exitCode: false,
   useSSL: false,
   threshold: 0.1,
   batch: false,
